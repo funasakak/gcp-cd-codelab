9a75c6e1c44d74e0febcbf7a6f28c83bc9748b2f
funasakakui <funasakakui@gmail.com>
2017-12-23 16:11:26 +0000

    new version

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-        io.WriteString(w, "Hello World! Merry Chrismas")
+        io.WriteString(w, "Hello World! Goodbye Chrismas")
 }
 
 func main() {
