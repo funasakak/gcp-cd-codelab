721682f583b501da3ed760de416634a340c2a411
funasakakui <funasakakui@gmail.com>
2017-12-24 14:53:57 +0000

    new

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-        io.WriteString(w, "Hello World! Happy BirthDay!!!")
+        io.WriteString(w, "Hello World! 20171225")
 }
 
 func main() {
